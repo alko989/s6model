45c8a9e404ef1f42078a4af602167ecddf585852
alko <alko@aqua.dtu.dk>
2014-06-13 16:44:19 +0200

    Added s6model.R file with the package documentation.

---
+++ R/s6model.R
@@ -0,0 +1,11 @@
+#' Data poor stock assessment using a single species size based model in steady state
+#'
+#'  
+#'
+#' 
+#' @references Kokkalis, A., Nielsen, A., Thygesen U. H. and K. H. Andersen. (In review) Reliability of fisheries reference points estimation for data-poor stocks, Fisheries research
+#' @references Andersen, K. H., & Beyer, J. E. (2013). Size structure, not metabolic scaling rules, determines fisheries reference points. Fish and Fisheries. doi:10.1111/faf.12042
+#' 
+#' @docType package
+#' @name s6model
+NULL
