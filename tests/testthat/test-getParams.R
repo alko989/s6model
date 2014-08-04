8c4c13b9b6429313f76fc69cff3df1d332530968
alko <alko@aqua.dtu.dk>
2014-08-04 18:20:49 +0200

    Updated .gitignore, removed warnings from tests [skip ci]

--- tests/testthat/test-getParams.R
+++ tests/testthat/test-getParams.R
@@ -5,9 +5,9 @@ test_that("getParams reads parameters correctly", {
     p1 <- parameters()
     p2 <- p1
     p2@logeta_F <- log(0.01 / p1@scaleeta_F)
-    expect_warning(getParams(p2))
-    expect_equal(getParams(p2)$eta_F, exp(p1@logeta_F)*p1@scaleeta_F)
-    expect_equal(getParams(p2)$p@logeta_F, p1@logeta_F)
-    expect_equal(getParams(p2)$p, p1)
+    expect_warning(gpp2 <- getParams(p2))
+    expect_equal(gpp2$eta_F, exp(p1@logeta_F)*p1@scaleeta_F)
+    expect_equal(gpp2$p@logeta_F, p1@logeta_F)
+    expect_equal(gpp2$p, p1)
     expect_equal(getParams(p1)$p, p1)
 })
