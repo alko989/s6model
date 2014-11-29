8342305ea0c99286649566df5af1258f074d2908
alko <alko@aqua.dtu.dk>
2014-11-29 16:56:33 +0100

    Added relative recruitment (R/Rmax)

--- src/s6model.cpp
+++ src/s6model.cpp
@@ -7,6 +7,7 @@ Type objective_function<Type>::operator() ()
   DATA_VECTOR(freq);
   DATA_SCALAR(n);
   DATA_SCALAR(epsilon_a);
+  DATA_SCALAR(epsilon_r);
   DATA_SCALAR(A);
   DATA_SCALAR(eta_m);
   DATA_SCALAR(meanloga);
@@ -29,9 +30,11 @@ Type objective_function<Type>::operator() ()
   ADREPORT(Wfs);
   ADREPORT(a);
   ADREPORT(sigma);
-  Type cumsum, nc, w, psi_m, psi_F, g, m, N;
+  Type cumsum, nc, w, psi_m, psi_F, g, m, N, wr, alpha, ssb;
+  wr = 0.001;
   cumsum=0.0;
-  nc = 0.0;  
+  nc = 0.0;
+  ssb = 0.0;
   for(int j=0; j<nwc; j++) {
     w = binsize * (j + 1);
     psi_m = 1 / (1 + pow(w / (Winf * eta_m), -10));
@@ -39,10 +42,14 @@ Type objective_function<Type>::operator() ()
     g = A * pow(w, n) * (1 - pow(w / Winf, 1 - n) * (epsilon_a + (1 - epsilon_a) * psi_m));
     m = a * A * pow(w, n-1);
     cumsum += (m + Fm *  psi_F) / g * binsize; 
-    N = exp(-cumsum) / g;
+    N = exp(-cumsum) / g / wr;
+    alpha = epsilon_r * (1 - epsilon_a) * A * pow(Winf, n-1) / wr;
+    ssb += psi_m  * N * w * binsize;
     Nvec(j) = N * Fm * psi_F;
     nc += Nvec(j) * binsize;
   }
+  Type Rrel = 1 - (pow(Winf, 1-n) * wr) / (epsilon_r * (1 - epsilon_a) * A * ssb);
+  N = N * Rrel;
   Type ff = freq.sum() * binsize;
   Type nll=0.0;
   for(int i=0; i<nwc; i++) {
@@ -59,6 +66,7 @@ Type objective_function<Type>::operator() ()
   REPORT(Nvec);
   REPORT(freq);
   REPORT(residuals);
+  REPORT(Rrel);
   return nll;
 }
 
