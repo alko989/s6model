6c46224448e38b34f3355e4004497404b5c51cd1
alko <alko@aqua.dtu.dk>
2015-01-13 15:47:20 +0100

    Fixed small bug in addWeight function

--- R/assessment.R
+++ R/assessment.R
@@ -32,7 +32,7 @@ l2w <- function(l, a, b) {
 
 addWeight <- function(df, a, b, lengthcol = "Length") {
   if(is.null(df$Weight))
-    df$Weight <- l2w(l = df[lengthcol], a = a, b = b)
+    df$Weight <- l2w(l = df[[lengthcol]], a = a, b = b)
   else
     warning("Column `Weight` exists, the original data frame `df` is returned")
   df
