f75d451107de324e6ce2b4348d4b0099649b67bf
alko <alko@aqua.dtu.dk>
2015-02-19 18:13:48 +0100

    Not all CI results are saved

--- R/assessment.R
+++ R/assessment.R
@@ -78,7 +78,7 @@ getCI <- function (inputData, ests, a.mean, a.sd, nsample, winf.ubound, yield, p
   err <- sapply(ci, function(x) attr(x, "err"))
   errmsg <- sapply(ci, function(x) attr(x, "errmsg"))
   notConv <- sapply(ci, function(x) attr(x, "notConv"))
-  allResults <- lapply(ci, function(x) attr(x, "results"))
+  ## allResults <- lapply(ci, function(x) attr(x, "results"))
   as <-  lapply(ci, function(x) attr(x, "as"))
   nms <- names(ci[[1]])
   ci <- lapply(nms, function(nm) lapply(ci, function(d) d[[nm]]))
@@ -90,7 +90,7 @@ getCI <- function (inputData, ests, a.mean, a.sd, nsample, winf.ubound, yield, p
   })
   ci <- setNames(ci, nms)
   structure(ci, alims = alims, as = as, reps = reps, notConv = notConv, nerr = nerr, 
-            err = err, errmsg = errmsg, allResults = allResults)
+            err = err, errmsg = errmsg) ##, allResults = allResults)
 }
 
 ##' @export
