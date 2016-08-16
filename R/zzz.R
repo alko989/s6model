bb39da086d8ec3a999795d9c297caa255dbf948d
Alexandros Kokkalis <alko@aqua.dtu.dk>
2016-08-16 18:46:13 +0200

    Added combined TMB file for single years and time series

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 #' @useDynLib s6model
 #' @useDynLib s6modelts
 #' @useDynLib calcFmsy
+#' @useDynLib s6modelcomb
 .onAttach <- function(lib, pkg) {
    packageStartupMessage("Loading ", getVersion(),"\n")  
  }
@@ -9,5 +10,6 @@
   library.dynam.unload("s6model", lib)
   library.dynam.unload("s6modelts", lib)
   library.dynam.unload("calcFmsy", lib)
+  library.dynam.unload("s6modelcomb", lib)
 }
 
