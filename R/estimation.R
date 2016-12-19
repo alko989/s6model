8352683f12319a74981695730f930fd7273cf3e4
Alexandros Kokkalis <alko@aqua.dtu.dk>
2016-12-19 14:24:56 +0100

    Removed old (non TMB) estimation method

--- R/estimation.R
+++ R/estimation.R
@@ -1,192 +1,38 @@
-#' The negative log likelihood for given parameters and data
-#' 
-#' \code{minimizeme} is mainly used by an optimizer (e.g. \code{optim}) 
-#' to estimate parameters.
-#' 
-#' 
-#' @param theta Numeric vector of *transformed* parameter values
-#' @param data Numeric vector, or \code{data.frame} with columns named Weight and Freq. The observed values, fished individuals in grams
-#' @param names String vector. Contains the names of the parameter vector theta.
-#' @param fixed.names String vector. Names of constants.
-#' @param fixed.vals Numeric vector. Transformed values of constants.
-#' @param isSurvey Logical. If TRUE the observations are assumed to be from a survey.
-#' @return Numeric scalar. The negative log likelihood for the given parameters and observations.
-#' @author alko
-#' @keywords optimize
-#' @note If data is a list containing both sample and df, the \code{data.frame} df will be used.
-#' @examples 
-#'  
-#' \dontrun{
-#' ## Simulate some data with default parameter values and fishing mortality Fm = 0.3
-#' sim <- simulateData3(params = parameters("Fm", 0.3, transformed=FALSE))
+
+#' Title
 #'
-#' ## Plotting the negative log likelihood for different values of Fm
-#' Fm <- seq(0.1, 1, 0.05) 
-#' Fm.transformed <- log(Fm / 0.25)
-#' nll <- sapply(Fm.transformed, function(x) minimizeme(theta = x, data = sim$sample, names = "Fm"))
-#' plot(Fm, nll, type="l") 
+#' @param df 
+#' @param n 
+#' @param epsilon_a 
+#' @param epsilon_r 
+#' @param A 
+#' @param eta_m 
+#' @param a 
+#' @param Winf 
+#' @param sigma 
+#' @param u 
+#' @param sdloga 
+#' @param winf.ubound 
+#' @param Wfs 
+#' @param verbose 
+#' @param map 
+#' @param random 
+#' @param isSurvey 
+#' @param eta_S 
+#' @param usePois 
+#' @param totalYield 
+#' @param ... 
 #'
-#' ## Using optimise to estimate one parameter
-#' est <- optimise(f = minimizeme, interval=c(0.01, 1), data=sim$sample, names="Fm")
-#' est.Fm <- exp(est$minimum) * 0.25
-#' abline(v=est.Fm)
-#' mtext(paste("Estimated Fm = ", round(est.Fm, 2)), at=est.Fm)
-#' }
-#' 
-#' @export minimizeme
-#' @rdname minimizeme
-minimizeme <- function(theta, data, names, fixed.names=c(), fixed.vals=c(), isSurvey=FALSE) {
-  params <- parameters(c(names, fixed.names), c(theta, fixed.vals))  
-  if(is(data, "data.frame")) {
-    return(with(getParams(params,isSurvey),
-                sum( - data$Freq * log(pdfN.approx(data$Weight)) )))
-  } else if(is(data, "numeric")) {
-    return(with(getParams(params,isSurvey), sum(-log(pdfN.approx(data)))))
-  } else {
-    stop("data appears not to be numeric vector or data.frame")
-  }
-}
-
-
-
-#' Estimates parameters using the given data and maybe some parameters
-#' 
-#' Parameter estimation minimizing the negative log likelihood, using the
-#' nlminb function. \code{estimateParam} uses data from commercial catches or surveys.
-#' \code{estimateMultidata} uses both sources
-#' 
-#' 
-#' @param names string vector, the parameters to be estimated.
-#' @param data numeric vector, or \code{data.frame} with columns Weight and Freq, or \code{list} with a numeric vector named `sample` or a \code{data.frame} named `df`.
-#' Weight of individual fish (vector) or frequencies per weight class \code{data.frame}.
-#' @param start numeric vector, initial values of the parameters.
-#' @param lower The lower bound for the parameter estimation.
-#' @param upper The upper bound for the parameter estimation.
-#' @param fixed.names String vector. Names of constants.
-#' @param fixed.vals Numeric vector. Transformed values of constants.
-#' @param fixed.transformed Logical. If FALSE the constants are not transformed.
-#' @param plotFit logical, if TRUE a plot is produced with the fited pdf and the
-#' kernel density estimate of the data.
-#' @param isSurvey logical, if TRUE the data are assumed to be from a survey.
-#' @param verbose logical, if TRUE the estimated confidence intervals are printed.
-#' @param useTMB logical, if TRUE TMB is used for parameter estimation
-#' @param ... Additional named arguments passed to plotFit
-#' @return \code{link{Parameters}} object, containing the estimated parameters.
-#' @note If data is a list containing both sample and df, the \code{data.frame} df will be used.
-#' @author alko
-#' @keywords optimize
+#' @return
+#' @export
+#'
 #' @examples
-#' 
-#' ## Simulate some data
-#' sam <- simulateData3(params=parameters("a", 0.5, transformed=FALSE))
-#' 
-#' ## Estimate the a parameter and see the fitted plot
-#' estimateParam(names="a", data=sam$sample, plotFit=TRUE)
-#' @rdname estimateParam
-#' @export estimateParam
-estimateParam <-
-  function(names = c("Fm", "Winf", "Wfs"),
-           data=simulateData3(parameters(), samplesize=1000),
-           start= rep(0.5, length(names)),
-           lower=rep(-Inf, length(names)), upper=rep(Inf, length(names)),
-           fixed.names=c(), fixed.vals=numeric(0), fixed.transformed = TRUE,
-           plotFit=FALSE, isSurvey=FALSE, verbose=getOption("verbose"), useTMB = TRUE, ...) {
-    p <- parameters()
-
-    if(is(data, "list")) {
-      if("df" %in% names(data)) {
-        data <- data$df
-      } else if ("sample" %in% names(data)) {
-        data <- data$sample
-      } else stop("`data` is a list not containing an element named sample or df.")
-    }
-
-    start[which(names == "Winf")] <- 
-      ifelse(is(data, "data.frame"), (max(data$Weight) + 1), (max(data) + 1))
-
-    if( ! fixed.transformed) {
-      fixed.vals <- log(fixed.vals)
-    }
-    
-    useapply <- if(require(parallel)) mclapply else lapply
-    
-    sd <- mean <- rep(0, length(names))
-    
-    estim <- nlminb(log(start), minimizeme, data=data, names=names,
-                    fixed.names=fixed.names, fixed.vals=fixed.vals,
-                    lower=lower, upper=upper, isSurvey=isSurvey)
-    if(estim$convergence != 0) warning(estim$message)
-    
-    res <- estim$par
-    h <- hessian(minimizeme, estim$par, data=data,
-                 names=names, fixed.names=fixed.names,
-                 fixed.vals=fixed.vals,isSurvey=isSurvey)
-    s <- jacobian(minimizeme, estim$par, data=data,
-                  names=names, fixed.names=fixed.names,
-                  fixed.vals=fixed.vals,isSurvey=isSurvey)
-    vcm <- try(solve(h))
-    
-    ci <- matrix(rep(NA, length(names)*3),ncol=3, dimnames = list(names, c("Estimate","Lower", "Upper")))
-    st.er <- NA
-    if( ! is(vcm, "try-error")) {
-      st.er <- sqrt(diag(vcm)) 
-      ci <- cbind(exp(res), exp(outer(1.96 * st.er, c(-1,1), '*') + res))
-    }
-    
-    p <- parameters(c(names, fixed.names), c(t(simplify2array(res)), fixed.vals))
-    
-    if(plotFit) plotFit(p, data, ...)
-    if(verbose) print(ci)
-    return(structure(p, par=res, hessian=h, jacobian=s, st.er=st.er, ci=ci, 
-                     objective=estim$objective, convergence=estim$convergence, 
-                     nlminbMessage=estim$message, call=match.call(), version=getVersion()))
-  }
-
-##' @param surdata Same as data. Survey data.
-##' @param comdata Same as data. Commercial data.
-##' @rdname minimizeme
-minimizemeMultidata <- function(theta, surdata, comdata, names, fixed.names=c(), fixed.vals=c())
-{
-  params <- parameters(c(names, fixed.names), c(theta, fixed.vals))
-  return(with(getParams(params,isSurvey=TRUE), sum(-log(pdfN.approx(surdata)))) +
-           with(getParams(params,isSurvey=FALSE), sum(-log(pdfN.approx(comdata)))))
-}
-##' @param surdata Same as data. Survey data.
-##' @param comdata Same as data. Commercial data.
-##' @rdname estimateParam
-estimateMultidata <-
-  function(names = c("Fm", "Winf", "Wfs"),
-           surdata=simulateData3(parameters(), samplesize=1000, isSurvey=TRUE)$sample,
-           comdata=simulateData3(parameters(), samplesize=1000)$sample,
-           start= rep(0.5, length(names)), lower = rep(0.1, length(names)),
-           fixed.names=c(), fixed.vals=numeric(0),
-           plotFit=FALSE, ...) {
-    start[which(names == "Winf")] <- (max(surdata, comdata) + 1)
-    lower[which(names == "eta_F")] <- 0.0001
-    
-    useapply <- ifelse(require(parallel), mclapply, lapply)
-    sd <- mean <- rep(0, length(names))
-    
-    
-    estim <- nlminb(log(start), minimizemeMultidata, surdata=surdata, comdata=comdata,
-                    names=names, fixed.names=fixed.names, fixed.vals=fixed.vals,
-                    lower=log(lower))
-    if(estim$convergence != 0)
-      warning(estim$message)
-    res <- c(estim$par) ##, estim$convergence)
-    
-    p <- parameters(c(names, fixed.names), c(t(simplify2array(res)), fixed.vals))
-    if(plotFit) plotFit(p, data, ...)
-    return(p)
-  }
-
-##' @export
 estimate_TMB <- function(df, n=0.75, epsilon_a=0.8, epsilon_r=0.1, A=4.47, 
                          eta_m=0.25, a=0.22, Winf = NULL, sigma=NULL, u = 10,
                          sdloga = 0.7, winf.ubound = 2, Wfs = NULL,
                          verbose = FALSE, map = list(loga = factor(NA)), 
                          random = c(), isSurvey = FALSE, eta_S = NULL, usePois = TRUE,
-                         totalYield = NULL, perturbStartingVals = FALSE, ...) {
+                         totalYield = NULL, ...) {
   if (is.null(df)) return(NULL)
   if (! require(TMB)) stop("TMB is not installed! Please install and try again.")
   isTS <- is(df, "list")
