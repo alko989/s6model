3d7e147722d4357c8fa51d9ac94ead3f762b8bd9
Alex Kokkalis <alko@aqua.dtu.dk>
2023-02-27 13:51:07 +0100

    Finished s6Params

--- s6model/R/s6params_class.R
+++ s6model/R/s6params_class.R
@@ -1,18 +1,18 @@
 #' The s6params class and constructor
 #'
-#' s6params is an S3 class that contains all model parameters. The 
-#' \code{s6params} function is the constructor of the class. Convenient 
-#' functions are available to \code{plot}, draw \code{lines}, \code{simulate} 
-#' data, return a named list of all parameters (\code{as.list}) and get the 
+#' s6params is an S3 class that contains all model parameters. The
+#' \code{s6params} function is the constructor of the class. Convenient
+#' functions are available to \code{plot}, draw \code{lines}, \code{simulate}
+#' data, return a named list of all parameters (\code{as.list}) and get the
 #' mean parameters of a list of \code{s6params} objects.
 #'
-#' @param Winf numeric, asymptotic weight
+#' @param Winf numeric, asymptotic weight \eqn{W_\infty}
 #' @param Fm numeric, fishing mortality
 #' @param A numeric, growth parameter
 #' @param n numeric, exponent of consumption
-#' @param eta_F numeric, 50\% retention size, relative to asymptotic weight
-#' @param eta_m numeric, 50\% maturation size, relative to asymptotic weight
-#' @param eta_S numeric, 50\% retention size (survey), relative to asymptotic weight
+#' @param eta_F numeric, 50\% retention size, relative to \eqn{W_\infty}
+#' @param eta_m numeric, 50\% maturation size, relative to \eqn{W_\infty}
+#' @param eta_S numeric, 50\% retention size (survey), relative to \eqn{W_\infty}
 #' @param a numeric, physiological mortality
 #' @param epsilon_a numeric, allocation to activity
 #' @param epsilon_r numeric, recruitment efficiency
@@ -24,7 +24,7 @@
 #' @param base s6params object
 #'
 #' @author Alexandros Kokkalis <alko@aqua.dtu.dk>
-#' 
+#'
 #' @name s6params
 #' @aliases s6params-class
 #' @rdname s6params
@@ -32,10 +32,10 @@
 #' @examples
 #' ## Without any arguments gives a \code{s6params} object with default values
 #' s6params()
-#' 
+#'
 #' ## An object with some user selected values
 #' par1 <- s6params(Winf = 1000, Fm = 0.4, Wfs = 100)
-#'  
+#'
 #' ## Take a s6params object and change one parameter
 #' par <- s6params(Winf = (1000), a = (0.4), Fm = (0.2), Wfs = (100))
 #' changeMatsize <- par$clone()
@@ -55,12 +55,12 @@ s6params <- function(
     epsilon_a = 0.8,    # Allocation to maintenance
     epsilon_r = 0.1,    # Reproduction efficiency
     Wfs =       500,    # 50% retainment weight of commercial gear
-    u =         10, 
+    u =         10,
     ngrid =     1000,   # Grid points for Fmsy estimation
-    wl.a = 0.01,        # Weight-length relationship a,b: W = a L^b
-    wl.b = 3,                
+    wl.a =      0.01,   # Weight-length relationship a,b: W = a L^b
+    wl.b =      3,
     base = NULL) {
-  
+
   ## Get user defined parameters
   mc <- match.call()
   mcl <- as.list(mc[-1])
@@ -68,14 +68,17 @@ s6params <- function(
   usebase <- "base" %in% userargs
   excludeargs <- c("base", ".clone")
   userargs <- userargs[!userargs %in% excludeargs]
-  
-  if(usebase && ! is.s6params(base)) stop("base should be an s6params object", call. = FALSE)
-  
+
+  if (usebase && ! is.s6params(base))
+    stop("base should be an s6params object", call. = FALSE)
+
   for (arg in userargs) {
-    if (length(eval(mcl[[arg]])) != 1) stop(paste(arg, " should not be a vector"), call. = FALSE)
-    if (! is.finite(eval(mcl[[arg]]))) stop(paste(arg, " should be finite number"), call. = FALSE)
+    if (length(eval(parse(text = arg))) != 1)
+      stop(paste(arg, " should not be a vector"), call. = FALSE)
+    if (! is.finite(eval(parse(text = arg))))
+      stop(paste(arg, " should be finite number"), call. = FALSE)
   }
-  
+
   e <- new.env()
   ## Clone function
   e$.clone <- add_clone_fun(envir = e)
@@ -93,21 +96,20 @@ s6params <- function(
   e$ngrid <- ngrid
   e$wl.a <- wl.a
   e$wl.b <- wl.b
-  
+
   if ("base" %in% names(mcl)) {
-    mcl <- mcl[mcl!="base"]
+    mcl <- mcl[mcl != "base"]
     for (nm in setdiff(names(base), userargs)) {
       e[[nm]] <- base[[nm]]
     }
   }
-  
+
   if (e$Winf <= e$Wfs) {
     warning("The start of fishing occurs at a weight equal or greater than the asymptotic weight")
   }
-  
+
   e$.__env__ <- e
-  e$s6version <- s6model:::getVersion("s6model")
-  structure(e, class = "s6params")
+  structure(e, class = "s6params", s6version = getVersion("s6model"))
 }
 
 add_clone_fun <- function(envir) {
@@ -117,7 +119,7 @@ add_clone_fun <- function(envir) {
     res <- as.environment(as.list(envir))
     res$.__env__ <- res
     res$.clone <- add_clone_fun(res)
-    structure(res, class = "s6params")
+    structure(res, class = "s6params", s6version = attr(envir, "s6version"))
   }
 }
 
@@ -140,7 +142,7 @@ all.equal.s6params <- function(target, current) {
   names <- ls(envir = target)
   res <- TRUE
   for (n in names) {
-    if(target[[n]] != current[[n]]) return(FALSE)
+    if (target[[n]] != current[[n]]) return(FALSE)
   }
   res
 }
@@ -225,7 +227,7 @@ all.equal.s6params <- function(target, current) {
 #     mean(allvals, na.rm = TRUE)
 #   }), names(p)))
 # }
-# 
+#
 # ##' Takes a \code{s6params} object and changes its asymptotic weight
 # ##'
 # ##' The asymptotic weight is changed, along with the relative and absolute sizes of 50\% retention
@@ -237,7 +239,7 @@ all.equal.s6params <- function(target, current) {
 # ##' @rdname Winf-methods
 # ##' @export
 # setGeneric("Winf<-",function(object,value){standardGeneric("Winf<-")})
-# 
+#
 # ##' @rdname Winf-methods
 # ##' @aliases Winf<--methods, Winf<-,s6params-method
 # ##' @name Winfsetter
@@ -251,13 +253,13 @@ all.equal.s6params <- function(target, current) {
 #     object@logWfs <- log(eF * winf)
 #     return (object)
 #   })
-# 
+#
 # ##' @param object \code{s6params} object
 # ##' @export
 # ##' @docType methods
 # ##' @rdname Winf-methods
 # setGeneric("Winf", function(object) standardGeneric("Winf"))
-# 
+#
 # ##' @rdname Winf-methods
 # ##' @aliases Winf,s6params-method
 # setMethod("Winf",
@@ -302,34 +304,34 @@ format.s6params <- function(x, ...) {
   cat("|", rep("_", width), "|", rep("_", width), "|", rep("_", width), "|\n", sep="")
   cat("\n")
 }
-#' 
-#' 
-#' #' @param x a s6params object
-#' #' @param xlim the x limits (x1, x2) of the plot.
-#' #' @param ... Arguments passed to other methods.
-#' #' @note Additional arguments are passed to \code{\link{plot.default}} 
-#' #' (from plot) and to \code{\link{lines}} (from lines). From all other 
-#' #' functions the extra arguments are ignored.
-#' #'
-#' #' @export
-#' #' @rdname s6params
-#' plot.s6params <- function(x, xlim = c(0.001, 1), ...) {
-#'   p <- getParams(x)
-#'   plot.default(p$w / p$Winf, p$N * (p$w ^ 2), log="xy",
-#'                main = "Biomass with respect to relative weight",
-#'                xlab = "w/Winf", ylab = "Biomass",
-#'                xlim = xlim,
-#'                type = "l", ...)
-#' }
-#' 
-#' #' @export
-#' #' @rdname s6params
-#' lines.s6params <- function(x, ...){
-#'   p <- getParams(x)
-#'   lines(xy.coords(p$w / p$Winf, p$N * (p$w ^ 2)), ...)
-#' }
 #'
- 
+#'
+#' @param x a s6params object
+#' @param xlim the x limits (x1, x2) of the plot.
+#' @param ... Arguments passed to other methods.
+#' @note Additional arguments are passed to \code{\link{plot.default}}
+#' (from plot) and to \code{\link{lines}} (from lines). From all other
+#' functions the extra arguments are ignored.
+#'
+#' @export
+#' @rdname s6params
+plot.s6params <- function(x, xlim = c(0.001, 1), ...) {
+  p <- getParams(x)
+  plot.default(p$w / p$Winf, p$N * (p$w ^ 2), log="xy",
+               main = "Biomass with respect to relative weight",
+               xlab = "w/Winf", ylab = "Biomass",
+               xlim = xlim,
+               type = "l", ...)
+}
+
+#' @export
+#' @rdname s6params
+lines.s6params <- function(x, ...){
+  p <- getParams(x)
+  lines(xy.coords(p$w / p$Winf, p$N * (p$w ^ 2)), ...)
+}
+
+
 ##' @export
 ##' @rdname s6params
 as.list.s6params <- function(x, ...) {
@@ -338,11 +340,11 @@ as.list.s6params <- function(x, ...) {
 
 
 ##' Difference between objects
-##' 
+##'
 ##' @param x First object to compare
 ##' @export
 difference <- function(x, ...) {
-  UseMethod("difference") 
+  UseMethod("difference")
 }
 
 
@@ -365,7 +367,6 @@ if (dim(res)[2] == 0) return(FALSE)
 round(res, 4)
 }
 
-#' 
 #' ##' Visualizing fit of s6model
 #' ##'
 #' ##'
@@ -380,19 +381,19 @@ round(res, 4)
 #' ##' @docType methods
 #' ##' @rdname plotFit-methods
 #' ##' @docType methods
-#' ##' @export 
+#' ##' @export
 #' setGeneric("plotFit", function(object, data, add, ...){ standardGeneric ("plotFit") })
-#' 
+#'
 #' ##' @rdname plotFit-methods
 #' ##' @aliases plotFit,s6params,numeric,missing-method
 #' setMethod("plotFit", c("s6params", "numeric", "missing"),
 #'           function(object, data,...) {plotFit(object, data, FALSE,...)})
-#' 
+#'
 #' ##' @rdname plotFit-methods
 #' ##' @aliases plotFit,s6params,data.frame,missing-method
 #' setMethod("plotFit", c("s6params", "data.frame", "missing"),
 #'           function(object, data,...) {plotFit(object, data, FALSE,...)})
-#' 
+#'
 #' ##' @rdname plotFit-methods
 #' ##' @aliases plotFit,s6params,numeric,logical-method
 #' setMethod("plotFit", c("s6params", "numeric", "logical"),
@@ -409,7 +410,7 @@ round(res, 4)
 #'             hist(data, freq=FALSE, add=TRUE, breaks="FD")
 #'             invisible(NULL)
 #'           })
-#' 
+#'
 #' ##' @rdname plotFit-methods
 #' ##' @aliases plotFit,s6params,data.frame,logical-method
 #' setMethod("plotFit", c("s6params", "data.frame", "logical"),
@@ -432,7 +433,7 @@ round(res, 4)
 #'                    lty=1, lwd=2, seg.len=5)
 #'             invisible(NULL)
 #'           })
-#' 
+#'
 #' ##' Plots growth function
 #' ##'
 #' ##' @param object A \code{s6params} object
@@ -450,10 +451,10 @@ round(res, 4)
 #'             ylim.min <- max(p$g) / 100
 #'             ylim.max <- max(p$g)
 #'             ylim <- c( ylim.min - ylim.min * 0.01, ylim.max + ylim.max * 0.6)
-#'             plot(p$w / p$Winf, p$g, type="n", 
+#'             plot(p$w / p$Winf, p$g, type="n",
 #'                  xlab="",  log="xy", ylab="",
 #'                  xlim=c(0.01, 1), ylim = ylim, yaxt="n",xaxt="n", ...)
-#'             polygon(c(p$w/p$Winf, 1 ), c(p$psi_m, 0) * ylim.min * 3 + ylim.min, 
+#'             polygon(c(p$w/p$Winf, 1 ), c(p$psi_m, 0) * ylim.min * 3 + ylim.min,
 #'                     border="lightgrey",col="lightgrey")
 #'             lines(p$w / p$Winf, p$g, lwd=3)
 #'             abline(v = p$eta_m, lty = 2, lwd = 1.5)
@@ -472,7 +473,7 @@ round(res, 4)
 #'             axis(1, ticksat, labels = NA, tcl = 0.25, lwd = 0, lwd.ticks = 1)
 #'             invisible(NULL)
 #'           })
-#' 
+#'
 #' ##' Makes a plot of natural and fishing mortalities
 #' ##' @return Invisible \code{NULL}
 #' ##' @author alko
@@ -486,7 +487,7 @@ round(res, 4)
 #' setMethod("plotMortality", c("s6params"),
 #'           function(object, ...) {
 #'             p <- getParams(object)
-#'             plot(p$w / p$Winf, p$m , type = "n", lwd=2, 
+#'             plot(p$w / p$Winf, p$m , type = "n", lwd=2,
 #'                  xlab = "",  log = "x", ylab = "", xaxt = "n", yaxt = "n", ...)
 #'             title(xlab = expression(w / W[infinity]))
 #'             title(ylab = expression(Mortality ~ (y ^ {-1})), line = 1.4)
@@ -503,38 +504,39 @@ round(res, 4)
 #'             mtext(ys, side=2, line = 0.5, at = ys)
 #'             invisible(NULL)
 #'           })
-#' 
-#' 
-#' ##' @param object a \code{s6params} object.
-#' ##' @param nsim number of individuals in the simulated sample.
-#' ##' @param seed the seed that is passed to \code{\link{set.seed}}.
-#' ##' @param binsize numeric, the width of the weight classes in grams.
-#' ##' @param keepZeros logical, if TRUE keep bins with zero frequency.
-#' ##' @param ndataset integer, the number of datasets to simulate.
-#' ##' 
-#' ##' @note In \code{simulate}, all zero bins after the last non-zero bin are droped; even if \code{keepZeros} is TRUE.
-#' ##'
-#' ##' @export
-#' ##' @rdname s6params
-#' simulate.s6params <- function(object, nsim = 1000, seed = NULL, binsize = 100, 
-#'                               keepZeros = TRUE, ndataset = 1, ...) {
-#'   if (is.null(seed)) seed <- as.integer(rnorm(1, mean = 1e5, sd = 1e4))
-#'   set.seed(seed) 
-#'   p <- getParams(object)
-#'   l <- seq(binsize / 2, p$Winf - binsize / 2, binsize)
-#'   u <- seq(binsize + binsize / 2, p$Winf + binsize / 2, binsize)
-#'   pr <- mapply(function(low, up) {
-#'     integrate(p$pdfN.approx, lower = low, upper = up)$value},
-#'     l, u)
-#'   s <- rmultinom(ndataset, nsim, pr)
-#'   res <- lapply(seq(ndataset), function(ds) {
-#'     df <- data.frame(Weight = seq(binsize, p$Winf + binsize / 2, binsize), 
-#'                      Freq = s[, ds])
-#'     df <- structure(df, binsize = binsize)
-#'     rl <- rle(df$Freq)
-#'     df <- head(df, nrow(df) - if(tail(rl$values, 1) == 0) tail(rl$lengths, 1) else 0)
-#'     df
-#'   })
-#'   s6input(wf = res, surWF = list(), isSimulated = TRUE, trueParams = object, 
-#'           catch = rep(1, ndataset), ...)
-#' }
+
+
+#' @param object a \code{s6params} object.
+#' @param nsim number of individuals in the simulated sample.
+#' @param seed the seed that is passed to \code{\link{set.seed}}.
+#' @param binsize numeric, the width of the weight classes in grams.
+#' @param keepZeros logical, if TRUE keep bins with zero frequency.
+#' @param ndataset integer, the number of datasets to simulate.
+#' @param isSurvey logical, if \code{TRUE}, simulate survey data
+#'
+#' @note In \code{simulate}, all zero bins after the last non-zero bin are dropped; even if \code{keepZeros} is TRUE.
+#'
+#' @export
+#' @rdname s6params
+simulate.s6params <- function(object, nsim = 1000, seed = NULL, binsize = 100,
+                              keepZeros = TRUE, ndataset = 1, isSurvey = FALSE, ...) {
+  if (is.null(seed)) seed <- as.integer(rnorm(1, mean = 1e5, sd = 1e4))
+  set.seed(seed)
+  p <- getParams(object, isSurvey = isSurvey)
+  l <- seq(binsize / 2, p$Winf - binsize / 2, binsize)
+  u <- seq(binsize + binsize / 2, p$Winf + binsize / 2, binsize)
+  pr <- mapply(function(low, up) {
+    integrate(p$pdfN.approx, lower = low, upper = up)$value},
+    l, u)
+  s <- rmultinom(ndataset, nsim, pr)
+  res <- lapply(seq(ndataset), function(ds) {
+    df <- data.frame(Weight = seq(binsize, p$Winf + binsize / 2, binsize),
+                     Freq = s[, ds])
+    df <- structure(df, binsize = binsize)
+    rl <- rle(df$Freq)
+    df <- head(df, nrow(df) - if(tail(rl$values, 1) == 0) tail(rl$lengths, 1) else 0)
+    df
+  })
+  s6input(wf = res, surwf = NULL, isSimulated = TRUE, trueParameters = object,
+          catch = data.frame(Year = seq(ndataset), Catch = rep(1, ndataset)), ...)
+}
--- s6model/tests/testthat/test-s6params-class.R
+++ s6model/tests/testthat/test-s6params-class.R
@@ -1,55 +1,63 @@
-context("Parameters Class")
-
 test_that("A Parameters object is correctly initialized", {
   # Default values
   p <- s6params()
-  
+
   expect_true(is.s6params(p))
   expect_false(is.s6input(p))
-  
+
   a <- 0.123
   p.a <- s6params(a = a)
   expect_equal(p.a$a, a)
-  
+
   Winf <- 162534
   p.Winf <- s6params(Winf = Winf)
   expect_equal(p.Winf$Winf, Winf)
-  
+
   n <- 0.7
   p.n <- s6params(n = n)
   expect_equal(p.n$n, n)
 
   expect_warning(s6params(Winf = 1000, Wfs = 1000))
   expect_warning(s6params(Winf = 1000, Wfs = 1010))
-  
+
   expect_error(s6params(epsilon_a = "a"))
   expect_error(s6params(epsilon_r = "a"))
   expect_error(s6params(a = "a"))
   expect_error(s6params(Winf = "a"))
   expect_error(s6params(Fm = "a"))
-  
+
   expect_error(s6params(epsilon_a = c(1, 6, 7)))
   expect_error(s6params(epsilon_r = c(1, 6, 7)))
   expect_error(s6params(a = c(1, 6, 7)))
   expect_error(s6params(Winf = c(1, 6, 7)))
   expect_error(s6params(Fm = c(1, 6, 7)))
-  
+
 })
 
+
+test_that("Initalization works with parameters from local environments", {
+  x <- 1.2345
+  expect_error(p <- local({
+    FmVariable <- x
+    s6params(Fm = FmVariable)
+  }), NA)
+  expect_equal(p$Fm, x)
+  expect_equal(p$Fm, 1.2345)
+})
 
 test_that("Initialization of Parmaters object using base object", {
   expect_error(s6params(base = "a"))
   expect_error(s6params(base = list(Winf = 1000)))
   expect_error(s6params(base = 1))
-  
+
   a <- 0.123
   baseobj <- s6params(a = a)
-  
+
   expect_error(newobj <- s6params(base = baseobj), NA)
   expect_true(all.equal(baseobj, newobj))
-  
+
   expect_false(difference(baseobj, newobj))
-  
+
   expect_error(newobj <- s6params(a = 0.654, base = baseobj), NA)
   expect_equal(baseobj$a, 0.123)
   expect_equal(newobj$a, 0.654)
@@ -58,20 +66,19 @@ test_that("Initialization of Parmaters object using base object", {
 test_that("Cloning works as intended", {
   par1 <- s6params(Winf = 1000, Fm = 0.4, Wfs = 100)
   par2 <- par1$.clone()
-  
+
   expect_false(difference(par1, par2))
-  
+
   par3 <- par2$.clone()
-  
+
   expect_true(is.s6params(par1))
   expect_true(is.s6params(par2))
   expect_true(is.s6params(par3))
-  
+
   expect_true(all.equal(par1, par2))
   expect_true(all.equal(par1, par3))
   expect_true(all.equal(par2, par3))
-  
+
   expect_false(difference(par1, par3))
   expect_false(difference(par2, par3))
 })
-
